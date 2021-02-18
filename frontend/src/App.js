b08238f869bef0f12c014dacd389d2a2532f9d27
tony onserio <onseriocodes@gmail.com>
2021-02-17 20:40:44 -0600

    Initial React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button, Navbar } from 'react-bootstrap';
+
+const App = () => {
+  return (
+    <>
+      <Navbar bg='light'>
+        <Navbar.Brand>Khangadelic</Navbar.Brand>
+      </Navbar>
+    </>
+  );
+};
+
+export default App;
