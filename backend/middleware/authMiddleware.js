27925bd5c0bb2cacc8c0faaacf4d23ebf2e46f55
tony onserio <onseriocodes@gmail.com>
2021-05-11 11:55:23 -0500

    Admin Middleware & Get Users Endpoint

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 
+// PROTECT:
 const protect = asyncHandler(async (req, res, next) => {
   let token;
 
@@ -31,4 +32,14 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+// ADMIN:
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+};
+
+export { protect, admin };
